57b5d139ce6ee71ff2c4e745e88835d04bf9e1a6
koumen222 <koumenprive@gmail.com>
2026-01-13 18:41:18 +0100

    Retirer badge stock de l'image

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -200,12 +200,6 @@ function ProductPage() {
               display: 'block',
             }}
           />
-          {/* Badge */}
-          {productData?.stock && (
-            <div className="absolute top-4 right-4 bg-green-500 text-white px-4 py-2 rounded-full font-semibold shadow-lg z-10">
-              {productData.stock}
-            </div>
-          )}
       </div>
       
       {/* Deuxième image */}
